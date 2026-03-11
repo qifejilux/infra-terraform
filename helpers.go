4534fc5cf55b85189966836eb2a339b69cdd7f4d
qifejilux <262130654+qifejilux@users.noreply.github.com>
2026-03-11 23:55:57 +1000

    Optimize database query

--- helpers.go
+++ helpers.go
@@ -5,3 +5,9 @@ type ServerConfig struct {
     LogLevel string `json:"log_level"`
 }
 
+
+func ParseDate(dateStr string) (time.Time, error) {
+    layout := "2006-01-02T15:04:05Z"
+    return time.Parse(layout, dateStr)
+}
+
